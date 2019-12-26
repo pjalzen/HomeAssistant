1c5bc25eea309a0ac42a42ef70dc50b1c88b30ae
pjalzen <43309658+pjalzen@users.noreply.github.com>
2019-12-26 21:39:27 +0100

    another strava try

--- custom_components/strava/sensor.py
+++ custom_components/strava/sensor.py
@@ -1,436 +1,191 @@
-"""Sensor entity for the Strava platform."""
-
+"""Home Assistant component for Strava athlete activity statistics."""
+import asyncio
 from datetime import timedelta
+import json
 import logging
-from homeassistant.helpers.entity import Entity
+import os
+
+from aiohttp import web
+import voluptuous as vol
+
+from homeassistant.components.http import HomeAssistantView
+from homeassistant.components.sensor import (
+    ENTITY_ID_FORMAT, PLATFORM_SCHEMA)
+from homeassistant.const import LENGTH_METERS
+from homeassistant.core import callback
+from homeassistant.helpers.entity import Entity, async_generate_entity_id
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import HomeAssistantType
+
+REQUIREMENTS = ['stravalib==0.9.1']
+DEPENDENCIES = ['http']
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_ATHLETE = 'athlete'
-CONF_ACTIVITY = 'last_activity'
-CONF_CLUB = 'club'
-CONF_GEAR = 'gear'
-CONF_BIKE = 'bike'
-CONF_SHOE = 'shoe'
-CONF_STATS = 'stats'
-CONF_FIELDS = 'fields'
+SCAN_INTERVAL = timedelta(seconds=3600)
 
-DOMAIN = 'strava'
+CONF_CLIENT_ID = 'client_id'
+CONF_CLIENT_SECRET = 'client_secret'
 
-ICON = 'mdi:strava'
+STRAVA_CONFIG_PATH = 'strava.json'
 
-SCAN_INTERVAL = timedelta(minutes=1)
+DATA_CALLBACK = 'strava-callback'
 
-DEPENDENCIES = ['strava']
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_CLIENT_ID): cv.string,
+    vol.Required(CONF_CLIENT_SECRET): cv.string,
+})
 
-UNIT_MAPPING_FIELDS = {
-    'kilojoules': 'kJ',
-    'average_watts': 'W',
-    'device_watts': 'W',
-    'max_watts': 'W',
-    'average_heartrate': 'bpm',
-    'max_heartrate': 'bpm',
-    'calories': 'kC',
-    'member_count': 'Members',
-    'kudos_count': 'Kudos',
-    'comment_count': 'Comments',
-    'athlete_count': 'Athletes',
-    'follower_count': 'Followers',
-    'achievement_count': 'Achievements',
-    'friend_count': 'Friends',
-    'photo_count': 'Photos',
-    'total_photo_count': 'Photos',
-    'pr_count': 'Records',
-    'average_cadence': 'rpm'
-}
+ICON_RIDE = 'mdi:bike'
+ICON_RUN = 'mdi:run'
+ICON_SWIM = 'mdi:swim'
 
-ICON_MAPPING_FIELDS = {
-    'member_count': 'mdi:account-multiple',
-    'follower_count': 'mdi:account-multiple',
-    'distance': 'mdi:map-marker-distance',
-    'moving_time': 'mdi:timer',
-    'elapsed_time': 'mdi:timer',
-    'start_latlng': 'mdi:map-marker',
-    'end_latlng': 'mdi:map-marker',
-    'total_elevation_gain': 'mdi:elevation-rise',
-    'elev_high': 'mdi:elevation-rise',
-    'elev_low': 'mdi:elevation-decline',
-    'start_date': 'mdi:calendar-range',
-    'start_date_local': 'mdi:calendar-range',
-    'achievement_count': 'mdi:trophy',
-    'kudos_count': 'mdi:account-heart',
-    'comment_count': 'mdi:comment',
-    'athlete_count': 'mdi:account-multiple',
-    'friend_count': 'mdi:account-multiple',
-    'photo_count': 'mdi:image',
-    'total_photo_count': 'mdi:image',
-    'average_speed': 'mdi:speedometer',
-    'max_speed': 'mdi:speedometer',
-    'kilojoules': 'mdi:fire',
-    'average_watts': 'mdi:power-plug',
-    'device_watts': 'mdi:power-plug',
-    'max_watts': 'mdi:power-plug',
-    'weighted_average_watts': 'mdi:power-plug',
-    'max_heartrate': 'mdi:heart',
-    'average_heartrate': 'mdi:heart',
-    'calories': 'mdi:fire',
-    'suffer_score': 'mdi:hospital',
-    'pr_count': 'mdi:trophy'
-}
-
-ICON_MAPPING_ACTIVITY_TYPES = {
-    'AlpineSki': None,
-    'BackcountrySki': None,
-    'Canoeing': None,
-    'Crossfit': None,
-    'EBikeRide': None,
-    'Elliptical': None,
-    'Golf': 'mdi:golf',
-    'Handcycle': None,
-    'Hike': None,
-    'IceSkate': 'mdi:skate',
-    'InlineSkate': 'mdi:roller-skate',
-    'Kayaking': None,
-    'Kitesurf': None,
-    'NordicSki': None,
-    'Ride': 'mdi:bike',
-    'RockClimbing': None,
-    'RollerSki': None,
-    'Rowing': 'mdi:rowing',
-    'Run': 'mdi:run',
-    'Sail': 'mdi:ferry',
-    'Skateboard': None,
-    'Snowboard': None,
-    'Snowshoe': None,
-    'Soccer': 'mdi:soccer',
-    'StairStepper': 'mdi:stairs',
-    'StandUpPaddling': None,
-    'Surfing': None,
-    'Swim': 'mdi:swim',
-    'Velomobile': None,
-    'VirtualRide': 'mdi:bike',
-    'VirtualRun': 'mdi:run',
-    'Walk': 'mdi:walk',
-    'WeightTraining': 'mdi:dumbbell',
-    'Wheelchair': 'mdi:wheelchair-accessibility',
-    'Windsurf': None,
-    'Workout': None,
-    'Yoga': None
-}
+ATTR_YTD_RUN_DISTANCE = 'ytd_run_distance'
 
 
-async def async_setup_platform(hass, config, add_entities,
-                               discovery_info=None):
-    """Setups Strava sensors."""
-    data = hass.data.get(DOMAIN)
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+    """Authenticate to the Strava API."""
+    from stravalib.client import Client
 
-    athlete_id = config.get(CONF_ATHLETE)
-    gear_id = config.get(CONF_GEAR)
-    club_id = config.get(CONF_CLUB)
-    bike_id = config.get(CONF_BIKE)
-    shoe_id = config.get(CONF_SHOE)
+    hass.http.register_view(StravaAuthCallbackView())
 
-    fields = config.get(CONF_FIELDS) or []
-    stats = config.get(CONF_STATS) or []
-    activity = config.get(CONF_ACTIVITY) or []
+    client_id = config.get(CONF_CLIENT_ID)
+    client_secret = config.get(CONF_CLIENT_SECRET)
 
-    sensors = []
+    client = Client()
 
-    if bike_id:
-        gear_id = 'b{}'.format(bike_id)
-    elif shoe_id:
-        gear_id = 'g{}'.format(shoe_id)
+    config_path = hass.config.path(STRAVA_CONFIG_PATH)
 
-    if athlete_id:
-        if athlete_id == 'me':
-            athlete_id = None
+    @asyncio.coroutine
+    def _read_config():
+        if not os.path.isfile(config_path):
+            return None
+        with open(config_path, 'r') as auth_file:
+            config = json.load(auth_file)
+            if config['client_id'] == client_id:
+                return config['access_token']
 
-        for field in stats:
-            sensor = StravaAthleteStatsSensor(data, athlete_id, field)
-            sensors.append(sensor)
+    @asyncio.coroutine
+    def _write_config():
+        with open(config_path, 'w') as auth_file:
+            json.dump({
+                'client_id': client_id,
+                'access_token': client.access_token,
+            }, auth_file)
 
-        for field in activity:
-            sensor = StravaLastActivitySensor(data, athlete_id, field)
-            sensors.append(sensor)
+    @asyncio.coroutine
+    def _add_device():
+        strava = StravaSensor(hass, client)
+        yield from strava.async_update()
+        return async_add_devices([strava])
 
-        for field in fields:
-            sensor = StravaAthleteDetailsSensor(data, athlete_id, field)
-            sensors.append(sensor)
+    access_token = yield from _read_config()
+    if access_token is not None:
+        client.access_token = access_token
+        yield from _add_device()
+    else:
+        callback_url = '{}{}'.format(
+            hass.config.api.base_url, StravaAuthCallbackView.url)
+        authorize_url = client.authorization_url(
+            client_id=client_id, redirect_uri=callback_url)
 
-    elif gear_id:
-        for field in fields:
-            sensor = StravaGearSensor(data, gear_id, field)
-            sensors.append(sensor)
+        configurator = hass.components.configurator
+        request_id = configurator.async_request_config(
+            'Strava',
+            description='Authorization required for Strava account.',
+            link_name='Authorize Home Assistant',
+            link_url=authorize_url,
+            entity_picture='/local/images/logo_strava.png')
 
-    elif club_id:
-        for field in fields:
-            sensor = StravaClubSensor(data, club_id, field)
-            sensors.append(sensor)
+    @asyncio.coroutine
+    def initialize_callback(code):
+        """Handle OAuth callback from Strava authorization flow."""
+        client.access_token = client.exchange_code_for_token(
+            client_id=client_id, client_secret=client_secret, code=code)
+        yield from _write_config()
+        yield from _add_device()
+        configurator.async_request_done(request_id)
 
-    add_entities(sensors, True)
+    hass.data[DATA_CALLBACK] = initialize_callback
+    return True
+
+
+class StravaAuthCallbackView(HomeAssistantView):
+    """Web view that handles OAuth authentication and redirection flow."""
+    requires_auth = False
+    url = '/api/strava/callback'
+    name = 'api:strava:callback'
+
+    @callback
+    def get(self, request):  # pylint: disable=no-self-use
+        """Handle browser HTTP request."""
+        hass = request.app['hass']
+        params = request.query
+        response = web.HTTPFound('/states')
+
+        if 'code' not in params:
+            _LOGGER.error(
+                'Error authorizing Strava: %s',
+                params.get('error', 'invalid response'))
+        elif DATA_CALLBACK not in hass.data:
+            _LOGGER.error('Configuration request not found')
+        else:
+            code = params['code']
+            initialize_callback = hass.data[DATA_CALLBACK]
+            hass.async_add_job(initialize_callback(code))
+
+        return response
 
 
 class StravaSensor(Entity):
-    """Common base class for Strava sensor entities."""
+    """Sensor component for Strava athlete activity statistics."""
+    def __init__(self, hass: HomeAssistantType, strava_client):
+        self.hass = hass
+        self._client = strava_client
+        self._stats = None
 
-    def __init__(self, typ, field):
-        """Initialize Strava Sensor."""
-        self._data = None
-        self._type = typ
-
-        if '.' in field:
-            comps = field.split('.')
-
-            self._field = comps[0]
-            self._subfield = comps[1]
-        else:
-            self._field = field
-            self._subfield = None
-
-    async def async_update(self):
-        """Update state of Strava sensor."""
-        await self._data.update(self.hass)
-
-    @property
-    def _state(self):
-        """Overwritten by child classes."""
-        pass
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        from stravalib.model import ActivityTotals
-        from units.quantity import Quantity
-        from units import unit
-
-        attr = getattr(self._state, self._field, None)
-        if isinstance(attr, ActivityTotals):
-            attr = getattr(attr, self._subfield, None)
-
-        if isinstance(attr, Quantity):
-            if attr.unit == unit('m') and attr > unit('km')(10):
-                attr = unit('km')(attr)
-
-            return '%1.4g' % attr.num
-
-        if isinstance(attr, str) and len(attr) > 255:
-            return attr[:255]
-
-        return str(attr)
-
-    @property
-    def unit_of_measurement(self):
-        """Getter for sensor unit."""
-        from stravalib.model import ActivityTotals
-        from units.quantity import Quantity
-        from units import unit
-
-        field = self._field
-        attr = getattr(self._state, self._field, None)
-        if isinstance(attr, ActivityTotals):
-            attr = getattr(attr, self._subfield, None)
-            field = self._subfield
-
-        if isinstance(attr, Quantity):
-            if attr.unit == unit('m') and attr > unit('km')(10):
-                attr = unit('km')(attr)
-            return str(attr.unit)
-
-        if field in UNIT_MAPPING_FIELDS:
-            return UNIT_MAPPING_FIELDS[field]
-
-        return None
-
-    @property
-    def icon(self):
-        """Getter for entity icon."""
-        if self._field and self._field in ICON_MAPPING_FIELDS:
-            return ICON_MAPPING_FIELDS[self._field]
-
-        if self._subfield and self._subfield in ICON_MAPPING_FIELDS:
-            return ICON_MAPPING_FIELDS[self._subfield]
-
-        return ICON
-
-    @property
-    def available(self):
-        """Getter for entity availability state."""
-        return self._state is not None
-
-    @property
-    def unique_id(self):
-        """Getter for unique entity id."""
-        field = self._field
-        if self._subfield:
-            field += '_' + self._subfield
-
-        return 'strava_{}_{}_{}'.format(
-            self._type, self._data.id, field)
-
-    @property
-    def _name_prefix(self):
-        if self.available:
-            return self._state.name + ' '
-
-        return '{} {}: '.format(
-            self._type.replace('_', ' ').title(),
-            self._data.id)
+        athlete = self._client.get_athlete()
+        self._name = '{} {}'.format(athlete.firstname, athlete.lastname)
+        self.athlete_id = athlete.id
+        self.entity_id = async_generate_entity_id(
+            ENTITY_ID_FORMAT, 'strava_{}'.format(self.athlete_id), hass=hass)
 
     @property
     def name(self):
-        """Getter for entity name."""
-        name = self._name_prefix
-        name += self._field.replace('_', ' ').title()
-
-        if self._subfield:
-            name += " {}".format(
-                self._subfield.replace('_', ' ').title())
-
-        return name
-
-
-class StravaLastActivitySensor(StravaSensor):
-    """Entity of an Strava last acitivty sensor."""
-
-    def __init__(self, data, athlete_id, field):
-        """Initialize Strava last activity sensor."""
-        super().__init__('last_activity', field)
-
-        self._data = data.get_athlete(athlete_id)
+        return self._name
 
     @property
-    def _state(self):
-        return self._data.last_activity
+    def icon(self):
+        return ICON_RUN
 
     @property
-    def _name_prefix(self):
-        return 'Last Activity '
+    def unit_of_measurement(self):
+        return LENGTH_METERS
+
+    @property
+    def state(self):
+        if self._stats is None:
+            return 0
+        return self._stats.ytd_run_totals.distance.num
 
     @property
     def device_state_attributes(self):
-        """Getter for device state attributes."""
-        return {
-            'activity_id': self._state.id
-        }
+        """Return the athete stats attributes."""
+        attributes = {}
+        if self._stats is not None:
+            attributes.update({
+                ATTR_YTD_RUN_DISTANCE: self._stats.ytd_run_totals.distance.num,
+            })
+        return attributes
 
+    @asyncio.coroutine
+    def async_fetch_stats(self):
+        """Fetch athlete statistics from Strava."""
+        return self.hass.async_add_job(
+            self._client.get_athlete_stats, self.athlete_id)
 
-class StravaAthleteDetailsSensor(StravaSensor):
-    """Entity of an Strava athlete details sensor."""
-
-    def __init__(self, data, athlete_id, field):
-        """Initialize an Strava athlete sensor."""
-        super().__init__('athlete', field)
-
-        self._data = data.get_athlete(athlete_id)
-
-    @property
-    def _state(self):
-        return self._data.details
-
-    @property
-    def _name_prefix(self):
-        if self.available:
-            return self._data.details.firstname + ': '
-
-        if self._data.id:
-            return 'Athlete {}: '.format(self._data.id)
-
-        return 'Athlete: '
-
-    @property
-    def entity_picture(self):
-        """Getter for entity picture."""
-        if self.available:
-            return self._data.details.profile
-
-    @property
-    def device_state_attributes(self):
-        """Getter for device state attributes."""
-        return {
-            'athlete_id': self._data.details.id
-        }
-
-
-class StravaAthleteStatsSensor(StravaSensor):
-    """Entity of an Strava athlete statistics sensor."""
-
-    def __init__(self, data, athlete_id, field):
-        """Initialize an Strava athlete statistics sensor."""
-        super().__init__('stats', field)
-
-        self._data = data.get_athlete(athlete_id)
-
-    @property
-    def _state(self):
-        return self._data.stats
-
-    @property
-    def _name_prefix(self):
-        if self.available:
-            return self._data.details.firstname + ': '
-
-        if self._data.id:
-            return 'Athlete {}: '.format(self._data.id)
-
-        return 'Athlete: '
-
-    @property
-    def entity_picture(self):
-        """Getter for entity picture."""
-        if self._data.details:
-            return self._data.details.profile
-
-    @property
-    def device_state_attributes(self):
-        """Getter for device state attributes."""
-        return {
-            'athlete_id': self._data.details.id
-        }
-
-
-class StravaClubSensor(StravaSensor):
-    """Entity of an Strava club sensor."""
-
-    def __init__(self, data, club_id, field):
-        """Initialize an Strava club sensor."""
-        super().__init__('club', field)
-
-        self._data = data.get_club(club_id)
-
-    @property
-    def _state(self):
-        return self._data.club
-
-    @property
-    def entity_picture(self):
-        """Getter for entity picture."""
-        if self.available:
-            return self._state.profile_medium
-
-    @property
-    def device_state_attributes(self):
-        """Getter for device state attributes."""
-        return {
-            'club_id': self._state.id
-        }
-
-
-class StravaGearSensor(StravaSensor):
-    """Entity of an Strava gear sensor."""
-
-    def __init__(self, data, gear_id, field):
-        """Initialize an Strava gear sensor."""
-        super().__init__('gear', field)
-
-        self._data = data.get_gear(gear_id)
-
-    @property
-    def _state(self):
-        return self._data.gear
-
-    @property
-    def device_state_attributes(self):
-        """Getter for device state attributes."""
-        return {
-            'gear_id': self._state.id
-        }
+    @asyncio.coroutine
+    def async_update(self):
+        """Update current athlete statistics."""
+        self._stats = yield from self.async_fetch_stats()
